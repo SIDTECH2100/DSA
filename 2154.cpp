08a78d18715193b4af2818503d7b38c09e096e16
Siddharth Singh <siddharthsingh111222@gmail.com>
2025-11-19 11:27:25 +0530

    Add files via upload

---
+++ 2154.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        sort(nums.begin(), nums.end());
+        for (int num : nums) {
+            if (original == num) {
+                original *= 2;
+            }
+        }
+        return original;
+    }
+};
+
+***************************************************************************************************8
+
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        unordered_set<int> s(nums.begin(), nums.end());
+        while (s.count(original)) {
+            original *= 2;
+        }
+        return original;
+    }
+};
