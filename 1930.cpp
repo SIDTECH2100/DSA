7b1af344867821d5be365726e4272ecd6c80372d
Siddharth Singh <siddharthsingh111222@gmail.com>
2026-05-28 10:32:36 +0530

    Add files via upload

--- 1930.cpp
+++ 1930.cpp
@@ -1,35 +1,64 @@
 class Solution {
-public:
-    int countPalindromicSubsequence(string s) {
-        unordered_set<char> letters;
-        for (char c : s) {
-            letters.insert(c);
-        }
+    struct TrieNode {
+        int children[26];
+        int bestLen;
+        int bestIdx;
         
-        int ans = 0;
-        for (char letter : letters) {
-            int i = -1;
-            int j = 0;
+        TrieNode() {
+            fill(begin(children), end(children), -1);
+            bestLen = 1e9;
+            bestIdx = 1e9;
+        }
+    };
+
+public:
+    vector<int> stringIndices(vector<string>& wordsContainer, vector<string>& wordsQuery) {
+        vector<TrieNode> trie;
+        trie.emplace_back();
+        
+        for (int i = 0; i < wordsContainer.size(); i++) {
+            int len = wordsContainer[i].length();
+            int curr = 0;
             
-            for (int k = 0; k < s.size(); k++) {
-                if (s[k] == letter) {
-                    if (i == -1) {
-                        i = k;
-                    }
-                    
-                    j = k;
+            if (len < trie[curr].bestLen || (len == trie[curr].bestLen && i < trie[curr].bestIdx)) {
+                trie[curr].bestLen = len;
+                trie[curr].bestIdx = i;
+            }
+            
+            for (int j = len - 1; j >= 0; j--) {
+                int charIdx = wordsContainer[i][j] - 'a';
+                
+                if (trie[curr].children[charIdx] == -1) {
+                    trie[curr].children[charIdx] = trie.size();
+                    trie.emplace_back();
+                }
+                
+                curr = trie[curr].children[charIdx];
+                
+                if (len < trie[curr].bestLen || (len == trie[curr].bestLen && i < trie[curr].bestIdx)) {
+                    trie[curr].bestLen = len;
+                    trie[curr].bestIdx = i;
                 }
             }
+        }
+        
+        vector<int> ans;
+        ans.reserve(wordsQuery.size());
+        
+        for (const string& query : wordsQuery) {
+            int curr = 0;
+            int len = query.length();
             
-            unordered_set<char> between;
-            for (int k = i + 1; k < j; k++) {
-                between.insert(s[k]);
+            for (int j = len - 1; j >= 0; j--) {
+                int charIdx = query[j] - 'a';
+                if (trie[curr].children[charIdx] == -1) {
+                    break;
+                }
+                curr = trie[curr].children[charIdx];
             }
-            
-            ans += between.size();
+            ans.push_back(trie[curr].bestIdx);
         }
         
         return ans;
     }
-};
-
+}; 
